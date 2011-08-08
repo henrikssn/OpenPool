3f4194255df57a1cb0a479de16f4e4d7675f5785
Erik Henriksson <kontakt@erikhenriksson.se>
2011-08-08 16:37:45 +0200

    Added license

--- pages/signup/init.php
+++ pages/signup/init.php
@@ -1,4 +1,20 @@
 <?php
+# This file is part of OpenPool.
+#
+#    OpenPool is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    OpenPool is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with OpenPool.  If not, see <http://www.gnu.org/licenses/agpl-3.0.html>.
+?>
+<?php
     if(isset($_POST["username"]) && isset($_POST["password"])) {
         if (!new_user($_POST["username"], $_POST["password"])) {
             $error = "An error has occured, please try again.";
